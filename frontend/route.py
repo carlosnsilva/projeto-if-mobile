19184a46a5bf073984f46dc6fb8eef0e8bde1be9
Adrianderson Lira <adriandersonmusica@gmail.com>
2020-11-11 15:13:04 -0300

    Frontend atualizado

--- frontend/route.py
+++ frontend/route.py
@@ -18,24 +18,23 @@ porta = 5432
 
 app = Flask(__name__) # Inicializa a aplicação
 
-@app.route('/dado_requisito_1') # Cria uma rota
-def dado_requisito_1():
+@app.route('/') # Cria uma rota
+def index():
   return render_template('index.html')
 
-@app.route('/', methods=('GET', 'POST'))
-def requisito_1():
+@app.route('/gerarNumero', methods=('GET', 'POST'))
+def gerarNumero():
   conn = callBank(host, db, usr, senha, porta)
   cur = conn.cursor()
 
   if request.method == 'POST':
-    operadora = request.form['operadora']
-    estado = request.form['estado']
+    operadora = request.form.get('operadora')
+    estado = request.form.get('estado')
 
-    cur.execute("select * from gerar_numero(?, ?)",(operadora, estado))
+    cur.execute(f"select * from gerar_numero('{operadora}', '{estado}')")
     numero_new = cur.fetchall()
 
-    return render_template('dado_requisito_1.html', numero_new=numero_new)
-
+    return render_template('listaGerarNumero.html', numero_new=numero_new)
 
   cur.execute('select * from operadora')
   operadoras = cur.fetchall()
@@ -45,7 +44,109 @@ def requisito_1():
   
   cur.close()
 
-  return render_template('requisito_1.html', operadoras=operadoras, estados=estados)
+  return render_template('gerarNumero.html', operadoras=operadoras, estados=estados)
+
+
+
+@app.route('/oferecerNumero')
+def oferecerNumero():
+  return render_template('oferecerNumero.html')
+
+@app.route('/listaOferecerNumero')
+def listaOferecerNumero():
+  conn = callBank(host, db, usr, senha, porta)
+  cur = conn.cursor()
+
+  cur.execute('select * from numeros_disponiveis()')
+  numeros = cur.fetchall()
+
+  return render_template('listaOferecerNumero.html', numeros=numeros)
+
+
+@app.route('/gerarFaturas', methods=('GET', 'POST'))
+def gerarFaturas():
+  if request.method == 'POST':
+    conn = callBank(host, db, usr, senha, porta)
+    cur = conn.cursor()
+
+    ano = int(request.form.get('ano'))
+    mes = int(request.form.get('mes'))
+
+    cur.execute(f'call gerar_faturas({ano}, {mes})')
+    conn.commit()
+
+    conn = callBank(host, db, usr, senha, porta)
+    cur = conn.cursor()
+
+    if mes < 10:
+      mes = '0'+str(mes)
+
+    cur.execute(f"select * from fatura where referencia >= '{str(ano)}-{str(mes)}-01'")
+    faturas = cur.fetchall()
+    return render_template('listaGerarFaturas.html', faturas=faturas)
+
+  return render_template('gerarFaturas.html')
+
+
+
+@app.route('/gerarLigacoes', methods=('GET', 'POST'))
+def gerarLigacoes():
+  if request.method == 'POST':
+    conn = callBank(host, db, usr, senha, porta)
+    cur = conn.cursor()
+
+    ano = int(request.form.get('ano'))
+    mes = int(request.form.get('mes'))
+
+    # cur.execute(f'call povoar_ligacoes({ano}, {mes})')
+    # conn.commit()
+
+    conn = callBank(host, db, usr, senha, porta)
+    cur = conn.cursor()
+
+    if mes < 10:
+      mes = '0'+str(mes)
+
+    sql = f"""select * from ligacao li 
+              where li.data >= '{str(ano)}-{str(mes)}-01 00:00:00'
+              and li.data < (ultimo_dia('{str(ano)}-{str(mes)}-01') || ' 23:59:59')::timestamp"""
+
+    cur.execute(sql)
+    ligacoes = cur.fetchall()
+    return render_template('listaGerarLigacoes.html', ligacoes=ligacoes)
+
+  return render_template('gerarLigacoes.html')
+
+
+@app.route('/view1')
+def view1():
+  conn = callBank(host, db, usr, senha, porta)
+  cur = conn.cursor()
+
+  cur.execute('select * from vw_1')
+  view1 = cur.fetchall()
+  return render_template('view1.html', view1=view1)
+
+
+@app.route('/view2')
+def view2():
+  conn = callBank(host, db, usr, senha, porta)
+  cur = conn.cursor()
+
+  cur.execute('select * from vw_2')
+  view2 = cur.fetchall()
+  return render_template('view2.html', view2=view2)
+
+
+@app.route('/view3')
+def view3():
+  conn = callBank(host, db, usr, senha, porta)
+  cur = conn.cursor()
+
+  cur.execute('select * from vw_3')
+  view3 = cur.fetchall()
+  return render_template('view3.html', view3=view3)
+
 
 if __name__ == '__main__':
   app.run(debug=True) # Executa a aplicação
